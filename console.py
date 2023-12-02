dbc2ae2a6ecbdc601eced779bd8562c8a47dcd22
CletsyMedia <Cletussam12@yahoo.com>
2023-12-02 19:35:40 +0100

    Harnessing the cmd console

--- console.py
+++ console.py
@@ -5,12 +5,12 @@ import re as regexp
 from shlex import split
 from models import storage
 from models.base_model import BaseModel
+from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from models.amenity import Amenity
 
 
 def parse(arg):
@@ -29,4 +29,68 @@ def parse(arg):
         bracket_retl = [i.strip(",") for i in braces_lexer]
         bracket_retl.append(braces_tokens.group())
         return bracket_retl
-  
+
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the HolbertonBnB command interpreter.
+
+    Attributes:
+        prompt (str): The command prompt.
+    """
+
+    prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "Amenity",
+        "City",
+        "Place",
+        "Review"
+        "State",
+        "User",
+    }
+
+    def emptyline(self):
+        """Do nothing when receiving an empty line."""
+        pass
+
+    def default(self, arg):
+        """Default behavior for cmd module when input is invalid"""
+        argdictionary = {
+            "all": self.do_all,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "show": self.do_show,
+            "update": self.do_update
+        }
+        match = regexp.search(r"\.", arg)
+        if match is not None:
+            arglen = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = regexp.search(r"\((.*?)\)", arglen[1])
+            if match is not None:
+                command = [arglen[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdictionary.keys():
+                    call = "{} {}".format(arglen[0], command[1])
+                    return argdictionary[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
+
+    def do_quit(self, arg):
+        """Quit command to exit the program.
+
+        This method terminates the HolbertonBnB command interpreter and exits the program. It takes no arguments.
+
+        Returns:
+            bool: True to indicate that the program should exit.
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signals to exit the program.
+
+        This method is called when the user enters the EOF (End Of File) character (usually Ctrl-D) to signal the end of input. It terminates the HolbertonBnB command interpreter and exits the program. It takes no arguments.
+
+        Returns:
+            bool: True to indicate that the program should exit.
+        """
+        print("")
+        return True
