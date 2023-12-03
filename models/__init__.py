5d72e833af4ca1b7007373ac01a0dab97970fbdf
CletsyMedia <Cletussam12@yahoo.com>
2023-12-03 01:51:09 +0100

    Models package

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """Initialize the models package and create a FileStorage instance."""
-
-# Import the FileStorage class from the engine module
 from models.engine.file_storage import FileStorage
 
 # Create a FileStorage instance to manage object serialization/deserialization
