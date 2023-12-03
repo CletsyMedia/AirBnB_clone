e610bd3377b73444e0105fdd14dead334503a9f3
CletsyMedia <Cletussam12@yahoo.com>
2023-12-03 01:58:00 +0100

    Amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -3,3 +3,14 @@
 from models.base_model import BaseModel
 
 
+class Amenity(BaseModel):
+    """Represent an amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initialize Amenity instance."""
+        super().__init__(*args, **kwargs)
+        self.name = ""
