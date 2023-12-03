8b8388d0813f37008f84fba396f05b287a579172
CletsyMedia <Cletussam12@yahoo.com>
2023-12-03 22:54:39 +0100

    More testing

--- tests/test_console.py
+++ tests/test_console.py
@@ -18,20 +18,203 @@ Unittest classes:
 import os
 import sys
 import unittest
+from io import StringIO
+from unittest.mock import patch
 from models import storage
 from models.engine.file_storage import FileStorage
 from console import HBNBCommand
-from io import StringIO
-from unittest.mock import patch
 
 
-class TestHBNBCommandHelp(unittest.TestCase):
-    """Unittests for testing prompts of the HBNB command interpreter."""
+class HBNBCommandTestCase(unittest.TestCase):
+    """Base class for HBNBCommand unittests."""
+
+    def setUp(self):
+        """Set up HBNBCommand instance for testing."""
+        self.hbnb_cmd = HBNBCommand()
+
+
+class TestHBNBCommand_prompting(HBNBCommandTestCase):
+    """Unittests for testing prompting of the HBNB command interpreter."""
 
     def test_prompt_string(self):
-        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+        """Test that the command prompt is correctly set."""
+        self.assertEqual("(hbnb) ", self.hbnb_cmd.prompt)
 
-    def test_empty_line(self):
+    def test_empty_line_output(self):
+        """Test the output when an empty line is entered."""
         with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertFalse(self.hbnb_cmd.onecmd(""))
             self.assertEqual("", output.getvalue().strip())
+
+
+class TestHBNBCommandHelp(HBNBCommandTestCase):
+    """Unittests for HBNB command interpreter help messages."""
+
+    def help_quit(self):
+        """Test help message for quit command."""
+        help_msg = "Quit command to exit the program."
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("help quit"))
+            self.assertEqual(help_msg, output.getvalue().strip())
+
+    def help_create(self):
+        """Test help message for create command."""
+        help_msg = ("Usage: create <class>\n        "
+                    "Create a new class instance and print its id.")
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("help create"))
+            self.assertEqual(help_msg, output.getvalue().strip())
+
+    def help_EOF(self):
+        """Test help message for EOF command."""
+        help_msg = "EOF signal to exit the program."
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("help EOF"))
+            self.assertEqual(help_msg, output.getvalue().strip())
+            
+    def help_show(self):
+        help_msg = ("Usage: show <class> <id> or <class>.show(<id>)\n        "
+             "Display the string representation of a class instance of"
+             " a given id.")
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("help show"))
+            self.assertEqual(help_msg, output.getvalue().strip())
+            
+    def help_destroy(self):
+        help_msg = ("Usage: destroy <class> <id> or <class>.destroy(<id>)\n        "
+                    "Delete a class instance of a given id.")
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("help destroy"))
+            self.assertEqual(help_msg, output.getvalue().strip())
+
+    def help_all(self):
+        help_msg = ("Usage: all or all <class> or <class>.all()\n        "
+                    "Display string representations of all instances of a given class"
+                    ".\n        If no class is specified, displays all instantiated "
+                    "objects.")
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("help all"))
+            self.assertEqual(help_msg, output.getvalue().strip())
+
+    def help_count(self):
+        help_msg = ("Usage: count <class> or <class>.count()\n        "
+                    "Retrieve the number of instances of a given class.")
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("help count"))
+            self.assertEqual(help_msg, output.getvalue().strip())
+
+    def help_update(self):
+        help_msg = ("Usage: update <class> <id> <attribute_name> <attribute_value> or"
+                    "\n       <class>.update(<id>, <attribute_name>, <attribute_value"
+                    ">) or\n       <class>.update(<id>, <dictionary>)\n        "
+                    "Update a class instance of a given id by adding or updating\n   "
+                    "     a given attribute key/value pair or dictionary.")
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("help update"))
+            self.assertEqual(help_msg, output.getvalue().strip())
+
+    def help(self):
+        help_msg = ("Documented commands (type help <topic>):\n"
+                    "========================================\n"
+                    "EOF  all  count  create  destroy  help  quit  show  update")
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("help"))
+            self.assertEqual(help_msg, output.getvalue().strip())
+
+
+class TestHBNBCommandExit(HBNBCommandTestCase):
+    """Unittests for exiting from the HBNB command interpreter."""
+
+    def quit_exits(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertTrue(self.hbnb_cmd.onecmd("quit"))
+
+    def EOF_exits(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertTrue(self.hbnb_cmd.onecmd("EOF"))
+    
+
+class TestHBNBCommandCreate(HBNBCommandTestCase):
+    """Unittests for testing create from the HBNB command interpreter."""
+
+    @classmethod
+    def setUpClass(cls):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+        FileStorage.__objects = {}
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
+    def create_missing_class(self):
+        correct = "** class name missing **"
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("create"))
+            self.assertEqual(correct, output.getvalue().strip())
+
+    def create_invalid_class(self):
+        correct = "** class doesn't exist **"
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("create MyModel"))
+            self.assertEqual(correct, output.getvalue().strip())
+
+    def create_invalid_syntax(self):
+        correct = "*** Unknown syntax: MyModel.create()"
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("MyModel.create()"))
+            self.assertEqual(correct, output.getvalue().strip())
+        correct = "*** Unknown syntax: BaseModel.create()"
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("BaseModel.create()"))
+            self.assertEqual(correct, output.getvalue().strip())
+
+    def create_object(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("create BaseModel"))
+            self.assertLess(0, len(output.getvalue().strip()))
+            test_key = "BaseModel.{}".format(output.getvalue().strip())
+            self.assertIn(test_key, storage.all().keys())
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("create User"))
+            self.assertLess(0, len(output.getvalue().strip()))
+            test_key = "User.{}".format(output.getvalue().strip())
+            self.assertIn(test_key, storage.all().keys())
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("create State"))
+            self.assertLess(0, len(output.getvalue().strip()))
+            test_key = "State.{}".format(output.getvalue().strip())
+            self.assertIn(test_key, storage.all().keys())
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("create City"))
+            self.assertLess(0, len(output.getvalue().strip()))
+            test_key = "City.{}".format(output.getvalue().strip())
+            self.assertIn(test_key, storage.all().keys())
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("create Amenity"))
+            self.assertLess(0, len(output.getvalue().strip()))
+            test_key = "Amenity.{}".format(output.getvalue().strip())
+            self.assertIn(test_key, storage.all().keys())
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("create Place"))
+            self.assertLess(0, len(output.getvalue().strip()))
+            test_key = "Place.{}".format(output.getvalue().strip())
+            self.assertIn(test_key, storage.all().keys())
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("create Review"))
+            self.assertLess(0, len(output.getvalue().strip()))
+            test_key = "Review.{}".format(output.getvalue().strip())
+            self.assertIn(test_key, storage.all().keys())
+
+
+if __name__ == "__main__":
+    unittest.main()
