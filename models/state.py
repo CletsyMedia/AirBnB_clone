058c9e731c9e0cc6d2df10cb20c66b082d227e06
CletsyMedia <Cletussam12@yahoo.com>
2023-12-03 15:14:23 +0100

    State class inherit basemodel

---
+++ models/state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines the State class."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represent a state.
+
+    Attributes:
+        name (str): The name of the state.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initialize State instance."""
+        super().__init__(*args, **kwargs)
+        self.name = ""
