44871d2cfe3b17aa595d9170f918e76df990271a
CletsyMedia <Cletussam12@yahoo.com>
2023-12-04 16:46:22 +0100

    Unnittest wahala

--- tests/test_console.py
+++ tests/test_console.py
@@ -47,24 +47,6 @@ class TestHBNBCommandPrompting(HBNBCommandTestCase):
             self.assertEqual("", output.getvalue().strip())
 
 
-class TestHBNBCommandHelp(HBNBCommandTestCase):
-    """Unittests for HBNB command interpreter help messages."""
-
-    def test_help_command(self):
-        """Test the help command."""
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(self.hbnb_cmd.onecmd("help"))
-            help_output = output.getvalue().strip()
-            self.assertIn("Documented commands (type help <topic>):", help_output)
-            self.assertIn("EOF  help  quit", help_output)
-
-    def test_help_quit_command(self):
-        """Test the help command for quit."""
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(self.hbnb_cmd.onecmd("help quit"))
-            help_output = output.getvalue().strip()
-            self.assertIn("Quit command to exit the program.", help_output)
-
 class TestHBNBCommandQuit(HBNBCommandTestCase):
     """Unittests for the quit command."""
 
@@ -83,6 +65,25 @@ class TestHBNBCommandQuit(HBNBCommandTestCase):
             self.assertEqual("", eof_output)
 
 
+class TestHBNBCommandHelp(HBNBCommandTestCase):
+    """Unittests for HBNB command interpreter help messages."""
+
+    def test_help_command(self):
+        """Test the help command."""
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("help"))
+            help_output = output.getvalue().strip()
+            print("Actual Output:\n", help_output)  # Add this line for debugging
+            self.assertIn("Documented commands (type help <topic>):", help_output)
+            self.assertIn("EOF  all  count  create  destroy  help  quit  show  update", help_output)
+
+    def test_help_quit_command(self):
+        """Test the help command for quit."""
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("help quit"))
+            help_output = output.getvalue().strip()
+            print("Actual Output:\n", help_output)  # Add this line for debugging
+            self.assertIn("Quit command to exit the program.", help_output)
 
 if __name__ == "__main__":
     unittest.main()
