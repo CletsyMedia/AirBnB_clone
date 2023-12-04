28f580907629a1d1625a54e212f8ed0f9279d7d2
CletsyMedia <Cletussam12@yahoo.com>
2023-12-04 22:21:12 +0100

    More unittest progress

--- tests/test_console.py
+++ tests/test_console.py
@@ -225,13 +225,20 @@ class TestHBNBCommandShow(HBNBCommandTestCase):
             error_output = output.getvalue().strip()
             self.assertIn("** no instance can be found **", error_output)
 
-    def test_show_dot_notation_no_instance_found(self):
+    def test_show_no_instance_found_dot_notation(self):
         """Test show command with dot notation and no instance found."""
-        # Assuming there is no BaseModel instance with ID '102'
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(self.hbnb_cmd.onecmd("show BaseModel.102"))
-            error_output = output.getvalue().strip()
-            self.assertIn("** no instance can be found **", error_output)
+        correct = "** no instance can be found **"
+
+        classes = ["BaseModel", "User", "State",
+                   "City", "Amenity", "Place", "Review"
+        ]
+
+        for class_name in classes:
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(self.hbnb_cmd.onecmd(f"{class_name}.show(1)"))
+                self.assertEqual(correct, output.getvalue().strip())
+
+
 
 
 
