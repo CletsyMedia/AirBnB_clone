0f777f2c4c99aa80ab77b7d0724f59ffad761f27
CletsyMedia <Cletussam12@yahoo.com>
2023-12-04 17:21:40 +0100

    Unnittest wahala

--- tests/test_console.py
+++ tests/test_console.py
@@ -144,5 +144,40 @@ class TestHBNBCommandDestroy(HBNBCommandTestCase):
             self.assertIn("** no instance can be found **", error_output)  # Update this line
 
 
+class TestHBNBCommandUpdate(HBNBCommandTestCase):
+    """Unittests for the update command."""
+
+    def test_update_command(self):
+        """Test the update command."""
+        # Assuming there is a BaseModel instance with ID '999'
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("update BaseModel 999 name 'new_name'"))
+            update_output = output.getvalue().strip()
+            self.assertEqual("", update_output)
+
+    def test_update_invalid_class(self):
+        """Test update command with an invalid class."""
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("update InvalidClass 999 name 'new_name'"))
+            error_output = output.getvalue().strip()
+            self.assertIn("** class doesn't exist **", error_output)
+
+    def test_update_invalid_id(self):
+        """Test update command with an invalid ID."""
+        # Assuming there is no BaseModel instance with ID '102'
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("update BaseModel 102 name 'new_name'"))
+            error_output = output.getvalue().strip()
+            self.assertIn("** no instance found **", error_output)
+
+    def test_update_invalid_attribute(self):
+        """Test update command with an invalid attribute."""
+        # Assuming there is a BaseModel instance with ID '103'
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.hbnb_cmd.onecmd("update BaseModel 103 invalid_attribute 'new_value'"))
+            error_output = output.getvalue().strip()
+            self.assertIn("** no attribute found **", error_output)
+
+
 if __name__ == "__main__":
     unittest.main()
