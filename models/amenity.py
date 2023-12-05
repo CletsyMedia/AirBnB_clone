0e231d2241c333c0d43c841f4a3c951d8ea41b69
CletsyMedia <Cletussam12@yahoo.com>
2023-12-05 14:49:53 +0100

    Acing through unnittest like 🔪 cutting 🧈

--- models/amenity.py
+++ models/amenity.py
@@ -4,10 +4,10 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Represent an amenity.
+    """Represents an amenity service for lodging.
 
     Attributes:
-        name (str): The name of the amenity.
+        name (str): The descriptive name of the amenity.
     """
 
     name = ""
