9583cb1b4561ce8a17d4c8260199a8ddcfc026cf
CletsyMedia <Cletussam12@yahoo.com>
2023-12-05 14:24:05 +0100

    Acing through unnittest like 🔪 cutting 🧈

--- models/amenity.py
+++ models/amenity.py
@@ -4,10 +4,13 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Represent an amenity class.
+    """Represent an amenity.
 
     Attributes:
         name (str): The name of the amenity.
     """
 
-    name = ""
+    def __init__(self, *args, **kwargs):
+        """Initialize Amenity instance."""
+        super().__init__(*args, **kwargs)
+        self.name = ""
