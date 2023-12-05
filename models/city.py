5a8abec084fab6e00e39ef4e345fa2812db6183c
CletsyMedia <Cletussam12@yahoo.com>
2023-12-05 17:49:04 +0100

    Acing through unnittest like 🔪 cutting 🧈

--- models/city.py
+++ models/city.py
@@ -4,17 +4,12 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Represent a city.
+    """Represents a city in the project.
 
     Attributes:
         state_id (str): The state id.
         name (str): The name of the city.
     """
 
-    __slots__ = ['state_id', 'name']
-
-    def __init__(self, *args, **kwargs):
-        """Initialize City instance."""
-        super().__init__(*args, **kwargs)
-        self.state_id = ""
-        self.name = ""
+    state_id = ""
+    name = ""
