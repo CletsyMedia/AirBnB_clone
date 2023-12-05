d28a9b3da96f9db816cd109820164e3c86284af1
CletsyMedia <Cletussam12@yahoo.com>
2023-12-05 22:44:46 +0100

    Acing through unnittest like 🔪 cutting 🧈

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -36,11 +36,17 @@ class TestPlaceInstantiation(HBNBCommandTestCase):
     def test_updated_at_is_public_datetime(self):
         self.assertEqual(datetime, type(Place().updated_at))
 
-    # def test_city_id_is_public_class_attribute(self):
-    #     pl = Place()
-    #     self.assertEqual(str, type(Place.city_id))
-    #     self.assertIn("city_id", dir(pl))
-    #     self.assertNotIn("city_id", pl.__dict__)
+    def test_city_id_is_public_class_attribute(self):
+        pl = Place()
+        self.assertEqual(str, type(pl.city_id))
+        self.assertIn("city_id", dir(pl))
+        self.assertNotIn("city_id", pl.__dict__)
+        
+    def test_user_id_is_public_class_attribute(self):
+        pl = Place()
+        self.assertEqual(str, type(pl.user_id))
+        self.assertIn("user_id", dir(pl))
+        self.assertNotIn("user_id", pl.__dict__)
     
     def test_no_args_instantiates(self):
         self.assertEqual(Place, type(Place()))
@@ -50,6 +56,24 @@ class TestPlaceInstantiation(HBNBCommandTestCase):
 
     def test_id_is_public_str(self):
         self.assertEqual(str, type(Place().id))
+        
+    def test_name_is_public_class_attribute(self):
+        pl = Place()
+        self.assertEqual(str, type(pl.name))
+        self.assertIn("name", dir(pl))
+        self.assertNotIn("name", pl.__dict__)
+
+    def test_description_is_public_class_attribute(self):
+        pl = Place()
+        self.assertEqual(str, type(pl.description))
+        self.assertIn("description", dir(pl))
+        self.assertNotIn("desctiption", pl.__dict__)
+
+    def test_number_rooms_is_public_class_attribute(self):
+        pl = Place()
+        self.assertEqual(int, type(pl.number_rooms))
+        self.assertIn("number_rooms", dir(pl))
+        self.assertNotIn("number_rooms", pl.__dict__)
 
 if __name__ == "__main__":
     unittest.main()
