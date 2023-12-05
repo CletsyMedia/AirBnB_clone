a9a584b9f6a0e08e7f5eec9d851b776b9c372ab4
CletsyMedia <Cletussam12@yahoo.com>
2023-12-05 14:08:58 +0100

    Acing through unnittest like 🔪 cutting 🧈

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines unittests for models/amenity.py.
+
+Unittest classes:
+    TestAmenity_instantiation
+    TestAmenity_save
+    TestAmenity_to_dict
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.amenity import Amenity
+
+
+class TestAmenity_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the Amenity class."""
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(Amenity, type(Amenity()))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(Amenity(), models.storage.all().values())
+
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(Amenity().id))
+
+
+if __name__ == "__main__":
+    unittest.main()
