7109200e78295045390895c620129216c535247a
CletsyMedia <Cletussam12@yahoo.com>
2023-12-05 14:14:08 +0100

    Acing through unnittest like 🔪 cutting 🧈

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,30 +1,39 @@
 #!/usr/bin/python3
 """Defines unittests for models/amenity.py.
 
+This module contains a series of unittest classes to test the functionality
+of the HBNB console. Each unittest class focuses on specific aspects of the console's behavior.
+
 Unittest classes:
-    TestAmenity_instantiation
-    TestAmenity_save
-    TestAmenity_to_dict
+    TestAmenityInstantiation
+    TestAmenitySave
+    TestAmenityDict
 """
-import os
-import models
-import unittest
 from datetime import datetime
+import unittest
+import models
+import os
 from time import sleep
 from models.amenity import Amenity
 
 
-class TestAmenity_instantiation(unittest.TestCase):
+class TestAmenityInstantiation(unittest.TestCase):
     """Unittests for testing instantiation of the Amenity class."""
+
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(Amenity().created_at))
+
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(Amenity().updated_at))
 
     def test_no_args_instantiates(self):
         self.assertEqual(Amenity, type(Amenity()))
 
-    def test_new_instance_stored_in_objects(self):
-        self.assertIn(Amenity(), models.storage.all().values())
-
     def test_id_is_public_str(self):
         self.assertEqual(str, type(Amenity().id))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(Amenity(), models.storage.all().values())
 
 
 if __name__ == "__main__":
