9c7df9f135d367ec8fadf41fce833eef3ee6bda3
CletsyMedia <Cletussam12@yahoo.com>
2023-12-05 23:03:05 +0100

    Acing through unnittest like 🔪 cutting 🧈

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -53,13 +53,13 @@ class TestReviewInstantiation(HBNBCommandTestCase):
 
     def test_user_id_is_public_class_attribute(self):
         rv = Review()
-        self.assertEqual(str, type(Review.user_id))
+        self.assertEqual(str, type(rv.user_id))
         self.assertIn("user_id", dir(rv))
         self.assertNotIn("user_id", rv.__dict__)
 
     def test_text_is_public_class_attribute(self):
         rv = Review()
-        self.assertEqual(str, type(Review.text))
+        self.assertEqual(str, type(rv.text))
         self.assertIn("text", dir(rv))
         self.assertNotIn("text", rv.__dict__)
 
