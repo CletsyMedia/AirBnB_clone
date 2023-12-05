6132e136999c697e89ded3844a285199f0da0855
CletsyMedia <Cletussam12@yahoo.com>
2023-12-05 23:30:15 +0100

    Acing through unnittest like 🔪 cutting 🧈

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -12,10 +12,25 @@ import unittest
 from datetime import datetime
 from time import sleep
 from models.user import User
+from console import HBNBCommand
 
 
-class TestUser_instantiation(unittest.TestCase):
+class HBNBCommandTestCase(unittest.TestCase):
+    """Base class for HBNBCommand unittests."""
+
+    def setUp(self):
+        """Set up HBNBCommand instance for testing."""
+        self.hbnb_cmd = HBNBCommand()
+
+
+class TestUser_instantiation(HBNBCommandTestCase):
     """Unittests for testing instantiation of the User class."""
 
     def test_no_args_instantiates(self):
         self.assertEqual(User, type(User()))
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
