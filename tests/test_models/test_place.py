51bc3614ce81eb7613e99e306bbc26e1bcded145
CletsyMedia <Cletussam12@yahoo.com>
2023-12-05 22:51:01 +0100

    Acing through unnittest like 🔪 cutting 🧈

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -157,5 +157,26 @@ class TestPlaceInstantiation(HBNBCommandTestCase):
             Place(id=None, created_at=None, updated_at=None)
 
 
+class TestPlace_save(HBNBCommandTestCase):
+    """Unittests for testing save method of the Place class."""
+
+    @classmethod
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
+
 if __name__ == "__main__":
     unittest.main()
