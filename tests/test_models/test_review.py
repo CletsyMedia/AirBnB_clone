bd9538293e4f0c9042e8d821e652a1a4420f56cb
CletsyMedia <Cletussam12@yahoo.com>
2023-12-05 23:01:56 +0100

    Acing through unnittest like 🔪 cutting 🧈

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -51,6 +51,63 @@ class TestReviewInstantiation(HBNBCommandTestCase):
     def test_no_args_instantiates(self):
         self.assertEqual(Review, type(Review()))
 
+    def test_user_id_is_public_class_attribute(self):
+        rv = Review()
+        self.assertEqual(str, type(Review.user_id))
+        self.assertIn("user_id", dir(rv))
+        self.assertNotIn("user_id", rv.__dict__)
+
+    def test_text_is_public_class_attribute(self):
+        rv = Review()
+        self.assertEqual(str, type(Review.text))
+        self.assertIn("text", dir(rv))
+        self.assertNotIn("text", rv.__dict__)
+
+    def test_two_reviews_unique_ids(self):
+        rv1 = Review()
+        rv2 = Review()
+        self.assertNotEqual(rv1.id, rv2.id)
+
+    def test_two_reviews_different_created_at(self):
+        rv1 = Review()
+        sleep(0.05)
+        rv2 = Review()
+        self.assertLess(rv1.created_at, rv2.created_at)
+
+    def test_two_reviews_different_updated_at(self):
+        rv1 = Review()
+        sleep(0.05)
+        rv2 = Review()
+        self.assertLess(rv1.updated_at, rv2.updated_at)
+
+    def test_str_representation(self):
+        dt = datetime.today()
+        dt_repr = repr(dt)
+        rv = Review()
+        rv.id = "123456"
+        rv.created_at = rv.updated_at = dt
+        rvstr = rv.__str__()
+        self.assertIn("[Review] (123456)", rvstr)
+        self.assertIn("'id': '123456'", rvstr)
+        self.assertIn("'created_at': " + dt_repr, rvstr)
+        self.assertIn("'updated_at': " + dt_repr, rvstr)
+
+    def test_args_unused(self):
+        rv = Review(None)
+        self.assertNotIn(None, rv.__dict__.values())
+
+    def test_instantiation_with_kwargs(self):
+        dt = datetime.today()
+        dt_iso = dt.isoformat()
+        rv = Review(id="345", created_at=dt_iso, updated_at=dt_iso)
+        self.assertEqual(rv.id, "345")
+        self.assertEqual(rv.created_at, dt)
+        self.assertEqual(rv.updated_at, dt)
+
+    def test_instantiation_with_None_kwargs(self):
+        with self.assertRaises(TypeError):
+            Review(id=None, created_at=None, updated_at=None)
+
 
 
 
