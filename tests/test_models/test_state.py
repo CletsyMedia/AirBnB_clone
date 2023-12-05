6c44f29536c50779a043e1f0ad53928381d82456
CletsyMedia <Cletussam12@yahoo.com>
2023-12-05 23:09:20 +0100

    Acing through unnittest like 🔪 cutting 🧈

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines unittests for models/state.py.
+
+Unittest classes:
+    TestState_instantiation
+    TestState_save
+    TestState_to_dict
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.state import State
+
+
+class TestState_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the State class."""
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(State, type(State()))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(State(), models.storage.all().values())
+
+
+if __name__ == "__main__":
+    unittest.main()
