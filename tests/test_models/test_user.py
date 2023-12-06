0dc07471d8df7af84c4a2c81f09207d8e24c0fc4
CletsyMedia <Cletussam12@yahoo.com>
2023-12-06 07:16:53 +0100

    Acing through unnittest like 🔪 cutting 🧈

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -26,10 +26,32 @@ class HBNBCommandTestCase(unittest.TestCase):
 class TestUser_instantiation(HBNBCommandTestCase):
     """Unittests for testing instantiation of the User class."""
 
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(User(), models.storage.all().values())
+
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(User().id))
+
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(User().created_at))
+    
     def test_no_args_instantiates(self):
         self.assertEqual(User, type(User()))
 
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(User().updated_at))
 
+    def test_email_is_public_str(self):
+        self.assertEqual(str, type(User().email))
+
+    def test_password_is_public_str(self):
+        self.assertEqual(str, type(User().password))
+
+    def test_first_name_is_public_str(self):
+        self.assertEqual(str, type(User().first_name))
+
+    def test_last_name_is_public_str(self):
+        self.assertEqual(str, type(User().last_name))
 
 
 if __name__ == "__main__":
