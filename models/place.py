55bf1bff2a06d5b8e268a9f09fea4a2a476e7095
CletsyMedia <Cletussam12@yahoo.com>
2023-12-09 13:12:20 +0100

    updating place.py i don tire

--- models/place.py
+++ models/place.py
@@ -19,22 +19,15 @@ class Place(BaseModel):
         longitude (float): The longitude coordinates.
         amenity_ids (list): A list of Amenity ids.
     """
-    __slots__ = ["city_id", "user_id", "name", "description", "number_rooms",
-                 "number_bathrooms", "max_guest", "price_by_night", "latitude",
-                 "longitude", "amenity_ids"]
 
-    def __init__(self, *args, **kwargs):
-        """Initialize Place instance."""
-        super().__init__(*args, **kwargs)
-
-        self.city_id = ""
-        self.user_id = ""
-        self.name = ""
-        self.description = ""
-        self.number_rooms = 0
-        self.number_bathrooms = 0
-        self.max_guest = 0
-        self.price_by_night = 0
-        self.latitude = 0.0
-        self.longitude = 0.0
-        self.amenity_ids = []
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
